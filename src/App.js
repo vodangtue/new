609098867a837ce7cb2c8918d76a1115b6d34187
vodangtue <vdtue0611@gmail.com>
2025-04-17 22:16:34 +0700

    CHORE: Remove unused files to clean up codebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/Home';
